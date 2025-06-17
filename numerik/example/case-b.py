e144c78d87bca6828c306e6e26a01c44c49197c1
Mifey05 <mifey05@gmail.com>
2025-06-17 21:26:51 +0800

    Update case-b.py

--- numerik/example/case-b.py
+++ numerik/example/case-b.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from numerik import trapezoidal_integration, simpsons_integration  
 import math
 
